35f6d7de976f1f0786ea23302fe903fb7beb7102
Scott Arbeitman <scott.arbeitman@gmail.com>
2011-09-07 16:51:51 +1000

    Seems to be working

--- do_analysis.rb
+++ do_analysis.rb
@@ -12,13 +12,22 @@ def first_value(set)
   end
 end
 
+def asset(condition)
+  raise Error unless condition
+end
+
 mysql = Mysql.init()
 mysql.connect('mysql.trikeapps.com', "elec_analysis", "ieFae5yi", "electrodrive_analysis", 3306)
 
-regions = mysql.query("SELECT DISTINCT(region_name) FROM electrodrive_regions")
+#the result set has multiple months of sales data across a few years, so we need to rescale to have a projection for one year
+num_months = first_value(mysql.query("SELECT (DATEDIFF(max(STR_TO_DATE(order_date, '%m/%d/%Y')),min(STR_TO_DATE(order_date, '%m/%d/%Y'))) / 30.5) FROM sales WHERE LENGTH(order_date) > 0 AND state = 'VIC'")).to_f
+
+regions = mysql.query("SELECT DISTINCT(region_name) FROM electrodrive_regions ORDER BY LEFT(region_code,1)")
+
 
 should_sell = {}
 regions.each_hash do |region|
+  product_name = nil
   region_name = region["region_name"]
   should_sell[region_name] = {}
   products = mysql.query("SELECT DISTINCT(model) FROM sales")
@@ -32,28 +41,31 @@ regions.each_hash do |region|
       number_of_employees_result = mysql.query(
         "SELECT `sum(employees)` FROM region_segment_employees WHERE region_name = '#{region_name}' AND segment_name = '#{segment_name}' LIMIT 1"
       ) 
-      
+      print '-'
       coefficient_result = mysql.query(
         "SELECT coefficient_per_thousand FROM product_coefficients WHERE segment_name = '#{segment_name}' AND model = '#{product_name}' LIMIT 1"
       )
-      
+      print '/'
       coefficient_number = first_value(coefficient_result).to_f
       number_of_employees = first_value(number_of_employees_result).to_f
       
-      should_sell[region_name][product_name] += (coefficient_number * (number_of_employees.to_i / 1000))      
+      should_sell[region_name][product_name] += (coefficient_number * (number_of_employees.to_i / 1000) / num_months * 12)      
     end
-  end
-  puts "Finished #{region_name}"
+  end 
+  puts "Finished #{region_name}" 
 end
 
-csv =  CSV.open("~/Desktop/should_sell.csv", "wb")
+pp should_sell
 
-should_sell.each |region_name, products|
+mysql.query("TRUNCATE TABLE projected_sales")
+
+should_sell.each do |region_name, products| 
   products.each do |product, number|
-    csv << [region_name, product, number]
+    query = "INSERT INTO projected_sales VALUES ('#{region_name}','#{product}',#{number})"
+    puts query
+    mysql.query(query)
   end
 end
-
 
 
 
